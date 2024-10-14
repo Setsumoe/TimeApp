7d0c3ab03a21b5d5f19fa0769c7b4067f5aba79d
User <User@SETSUMOE>
2024-10-14 15:00:36 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(130, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(223, 78);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(203, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Berlin";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label3.Location = new System.Drawing.Point(620, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 48);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Узнать свой IP адрес";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 0;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label5.Location = new System.Drawing.Point(0, 28);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label5.Size = new System.Drawing.Size(109, 27);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "TIME :";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label6.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(0, 0);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label6.Size = new System.Drawing.Size(126, 27);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "DATE :";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label7.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label7.Location = new System.Drawing.Point(200, 266);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(79, 36);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "New York";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label8.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label8.Location = new System.Drawing.Point(204, 78);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 36);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Tokyo";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label9.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label9.Location = new System.Drawing.Point(204, 124);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 36);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "London";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label11
+            // 
+            this.label11.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label11.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.label11.Location = new System.Drawing.Point(204, 170);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(75, 36);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "Moscow";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 311);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetCity();
+        }
+        private const string ApiUrlEU = "http://worldtimeapi.org/api/timezone/Europe/";
+        private const string ApiUrlUS = "http://worldtimeapi.org/api/timezone/america/";
+        private const string ApiUrlAS = "http://worldtimeapi.org/api/timezone/Asia/";
+        
+        private const string ApiGetIP = "http://worldtimeapi.org/api/ip";
+
+        private void GetCity()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.Encoding = Encoding.UTF8;
+                    string json = client.DownloadString("http://ip-api.com/json/?lang=ru");
+                    dynamic result1 = JsonConvert.DeserializeObject(json);
+
+                    string locatecity = result1.city;
+                    label1.Text = "Ваш город : " + locatecity;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Ошибка при определении города: " + ex.Message;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label5.Text = DateTime.Now.ToLongTimeString();
+
+            label6.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void label2_Click(object sender, EventArgs e)
+        {
+            string city = "Berlin";
+            string timezoneUrl = ApiUrlEU + city;
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(timezoneUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        dynamic result = JsonConvert.DeserializeObject(json);
+
+                        string timezone = "UTC " + result.utc_offset;
+                        DateTime currentTime = DateTime.UtcNow;
+                        string timeString = currentTime.ToLongTimeString();
+
+                        DateTime parsedTime;
+                        if (DateTime.TryParse(timeString, out parsedTime))
+                        {
+                            DateTime newTime = parsedTime.AddHours(2);
+                            string formattedTime = newTime.ToLongTimeString();
+
+                            MessageBox.Show($"Город: {city}\nЧасовой пояс: {timezone}\nТекущее время: {formattedTime}");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка при получении данных");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+        private async void label3_Click(object sender, EventArgs e)
+        {
+            string timezoneUrl = ApiGetIP;
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(timezoneUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        dynamic result = JsonConvert.DeserializeObject(json);
+
+                        string getip = result.client_ip;
+
+
+                        MessageBox.Show($"Ваш IP-адрес: {getip}");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка при получении данных");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label5.Text = DateTime.Now.ToLongTimeString();
+
+
+            timer1.Start();
+        }
+
+        private async void label9_Click(object sender, EventArgs e)
+        {
+            string city = "London";
+            string timezoneUrl = ApiUrlEU + city;
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(timezoneUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        dynamic result = JsonConvert.DeserializeObject(json);
+
+                        string timezone = "UTC " + result.utc_offset;
+                        DateTime currentTime = DateTime.UtcNow;
+                        string timeString = currentTime.ToLongTimeString();
+
+                        DateTime parsedTime;
+                        if (DateTime.TryParse(timeString, out parsedTime))
+                        {
+                            DateTime newTime = parsedTime.AddHours(1);
+                            string formattedTime = newTime.ToLongTimeString();
+
+                            MessageBox.Show($"Город: {city}\nЧасовой пояс: {timezone}\nТекущее время: {formattedTime}");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка при получении данных");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+        private async void label7_Click(object sender, EventArgs e)
+        {
+            string city = "new_york";
+            string timezoneUrl = ApiUrlUS + city;
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(timezoneUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        dynamic result = JsonConvert.DeserializeObject(json);
+
+                        string timezone = "UTC " + result.utc_offset;
+                        DateTime currentTime = DateTime.UtcNow;
+                        string timeString = currentTime.ToLongTimeString();
+
+                        DateTime parsedTime;
+                        if (DateTime.TryParse(timeString, out parsedTime))
+                        {
+                            DateTime newTime = parsedTime.AddHours(-4);
+                            string formattedTime = newTime.ToLongTimeString();
+
+                            MessageBox.Show($"Город: {city}\nЧасовой пояс: {timezone}\nТекущее время: {formattedTime}");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка при получении данных");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+        private async void label8_Click(object sender, EventArgs e)
+        {
+            string city = "Tokyo";
+            string timezoneUrl = ApiUrlAS + city;
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(timezoneUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        dynamic result = JsonConvert.DeserializeObject(json);
+
+                        string timezone = "UTC " + result.utc_offset;
+                        DateTime currentTime = DateTime.UtcNow;
+                        string timeString = currentTime.ToLongTimeString();
+
+                        DateTime parsedTime;
+                        if (DateTime.TryParse(timeString, out parsedTime))
+                        {
+                            DateTime newTime = parsedTime.AddHours(9);
+                            string formattedTime = newTime.ToLongTimeString();
+
+                            MessageBox.Show($"Город: {city}\nЧасовой пояс: {timezone}\nТекущее время: {formattedTime}");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка при получении данных");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+        private async void label11_Click(object sender, EventArgs e)
+        {
+            string city = "Moscow";
+            string timezoneUrl = ApiUrlEU + city;
+
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(timezoneUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+                        dynamic result = JsonConvert.DeserializeObject(json);
+
+                        string timezone = "UTC " + result.utc_offset;
+                        DateTime currentTime = DateTime.UtcNow;
+                        string timeString = currentTime.ToLongTimeString();
+
+                        DateTime parsedTime;
+                        if (DateTime.TryParse(timeString, out parsedTime))
+                        {
+                            DateTime newTime = parsedTime.AddHours(3);
+                            string formattedTime = newTime.ToLongTimeString();
+
+                            MessageBox.Show($"Город: {city}\nЧасовой пояс: {timezone}\nТекущее время: {formattedTime}");
+                        }
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Ошибка при получении данных");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Ошибка: {ex.Message}");
+                }
+            }
+        }
+    }
+}
+
